1b82d6445236294907cdbd5b39d4fbe39b55f02a
Mannan Goyal <mannangoyal@luganodes.com>
2025-04-22 19:34:27 +0530

    fix: formatting

--- src/Pectra.sol
+++ src/Pectra.sol
@@ -2,78 +2,41 @@
 pragma solidity ^0.8.28;
 
 contract Pectra {
-    address public immutable consolidationTarget =
-        0x0000BBdDc7CE488642fb579F8B00f3a590007251;
-    address public immutable exitTarget =
-        0x00000961Ef480Eb55e80D19ad83579A64c007002;
-    event ConsolidationFailed(
-        string message,
-        address sender,
-        bytes failedPubkey
-    );
+    address public immutable consolidationTarget = 0x0000BBdDc7CE488642fb579F8B00f3a590007251;
+    address public immutable exitTarget = 0x00000961Ef480Eb55e80D19ad83579A64c007002;
+
+    event ConsolidationFailed(string message, address sender, bytes failedPubkey);
     event SwitchFailed(string message, address sender, bytes failedPubkey);
-    event ExecutionLayerExitFailed(
-        string message,
-        address sender,
-        bytes pubkey,
-        bytes amount
-    );
+    event ExecutionLayerExitFailed(string message, address sender, bytes pubkey, bytes amount);
+
     error InvalidTargetPubkeyLength(bytes invalidTargetPubkey);
 
     modifier onlySelf() {
-        require(
-            msg.sender == address(this),
-            "Unauthorized: Must be executed by the Owner"
-        );
+        require(msg.sender == address(this), "Unauthorized: Must be executed by the Owner");
         _;
     }
 
-    function batchConsolidation(
-        bytes[] memory sourcePubkeys,
-        bytes memory targetPubkey
-    ) external payable onlySelf {
+    function batchConsolidation(bytes[] memory sourcePubkeys, bytes memory targetPubkey) external payable onlySelf {
         uint256 batchSize = sourcePubkeys.length;
         require(batchSize >= 1, "At least one source validator required");
-        require(
-            batchSize <= 63,
-            "Number of source validators must be less than 64, since Max EB is 2048 ETH"
-        );
+        require(batchSize <= 63, "Number of source validators must be less than 64, since Max EB is 2048 ETH");
         if (targetPubkey.length != 48) {
             revert InvalidTargetPubkeyLength(targetPubkey);
         }
-        require(
-            msg.value % batchSize == 0,
-            "msg.value must be divisible by length of source validators"
-        );
-        require(
-            msg.value >= batchSize * 1 wei,
-            "1 wei per source validator is required"
-        );
+        require(msg.value % batchSize == 0, "msg.value must be divisible by length of source validators");
+        require(msg.value >= batchSize * 1 wei, "1 wei per source validator is required");
         uint256 amountPerTx = msg.value / batchSize;
 
         for (uint256 i = 0; i < batchSize; i++) {
             if (sourcePubkeys[i].length != 48) {
-                emit ConsolidationFailed(
-                    "Invalid source validator public key length",
-                    msg.sender,
-                    sourcePubkeys[i]
-                );
+                emit ConsolidationFailed("Invalid source validator public key length", msg.sender, sourcePubkeys[i]);
                 continue;
             }
 
-            bytes memory concatenated = abi.encodePacked(
-                sourcePubkeys[i],
-                targetPubkey
-            );
-            (bool success, ) = consolidationTarget.call{value: amountPerTx}(
-                concatenated
-            );
+            bytes memory concatenated = abi.encodePacked(sourcePubkeys[i], targetPubkey);
+            (bool success,) = consolidationTarget.call{value: amountPerTx}(concatenated);
             if (!success) {
-                emit ConsolidationFailed(
-                    "Consolidation failed",
-                    msg.sender,
-                    sourcePubkeys[i]
-                );
+                emit ConsolidationFailed("Consolidation failed", msg.sender, sourcePubkeys[i]);
                 continue;
             }
         }
@@ -83,33 +46,18 @@ contract Pectra {
         uint256 batchSize = pubkeys.length;
         require(batchSize >= 1, "At least one validator required");
         require(batchSize <= 200, "Number of validators must be less than 200");
-        require(
-            msg.value % batchSize == 0,
-            "msg.value must be divisible by length of validators"
-        );
-        require(
-            msg.value >= batchSize * 1 wei,
-            "1 wei per validator is required"
-        );
+        require(msg.value % batchSize == 0, "msg.value must be divisible by length of validators");
+        require(msg.value >= batchSize * 1 wei, "1 wei per validator is required");
         uint256 amountPerTx = msg.value / batchSize;
 
         for (uint256 i = 0; i < batchSize; i++) {
             if (pubkeys[i].length != 48) {
-                emit SwitchFailed(
-                    "Invalid validator public key length",
-                    msg.sender,
-                    pubkeys[i]
-                );
+                emit SwitchFailed("Invalid validator public key length", msg.sender, pubkeys[i]);
                 continue;
             }
 
-            bytes memory concatenated = abi.encodePacked(
-                pubkeys[i],
-                pubkeys[i]
-            );
-            (bool success, ) = consolidationTarget.call{value: amountPerTx}(
-                concatenated
-            );
+            bytes memory concatenated = abi.encodePacked(pubkeys[i], pubkeys[i]);
+            (bool success,) = consolidationTarget.call{value: amountPerTx}(concatenated);
             if (!success) {
                 emit SwitchFailed("Switch failed", msg.sender, pubkeys[i]);
                 continue;
@@ -121,50 +69,24 @@ contract Pectra {
         uint256 batchSize = data.length;
         require(batchSize >= 1, "At least one entry required");
         require(batchSize <= 200, "Number of validators must be less than 200");
-        require(
-            msg.value % batchSize == 0,
-            "msg.value must be divisible by length of validators"
-        );
-        require(
-            msg.value >= batchSize * 1 wei,
-            "1 wei per validator is required"
-        );
+        require(msg.value % batchSize == 0, "msg.value must be divisible by length of validators");
+        require(msg.value >= batchSize * 1 wei, "1 wei per validator is required");
         uint256 amountPerTx = msg.value / batchSize;
 
         for (uint256 i = 0; i < batchSize; i++) {
             if (data[i][0].length != 48) {
-                emit ExecutionLayerExitFailed(
-                    "Invalid validator public key length",
-                    msg.sender,
-                    data[i][0],
-                    data[i][1]
-                );
+                emit ExecutionLayerExitFailed("Invalid validator public key length", msg.sender, data[i][0], data[i][1]);
                 continue;
             }
             if (data[i][1].length != 8) {
-                emit ExecutionLayerExitFailed(
-                    "Invalid amount length",
-                    msg.sender,
-                    data[i][0],
-                    data[i][1]
-                );
+                emit ExecutionLayerExitFailed("Invalid amount length", msg.sender, data[i][0], data[i][1]);
                 continue;
             }
 
-            bytes memory concatenated = abi.encodePacked(
-                data[i][0],
-                data[i][1]
-            );
-            (bool success, ) = exitTarget.call{value: amountPerTx}(
-                concatenated
-            );
+            bytes memory concatenated = abi.encodePacked(data[i][0], data[i][1]);
+            (bool success,) = exitTarget.call{value: amountPerTx}(concatenated);
             if (!success) {
-                emit ExecutionLayerExitFailed(
-                    "Execution layer exit failed",
-                    msg.sender,
-                    data[i][0],
-                    data[i][1]
-                );
+                emit ExecutionLayerExitFailed("Execution layer exit failed", msg.sender, data[i][0], data[i][1]);
                 continue;
             }
         }
--- test/Pectra.t.sol
+++ test/Pectra.t.sol
@@ -8,8 +8,7 @@ contract PectraTest is Test {
     Pectra public pectra;
 
     // These addresses are hardcoded in the Pectra contract.
-    address constant consolidationTarget =
-        0x0000BBdDc7CE488642fb579F8B00f3a590007251;
+    address constant consolidationTarget = 0x0000BBdDc7CE488642fb579F8B00f3a590007251;
     address constant exitTarget = 0x00000961Ef480Eb55e80D19ad83579A64c007002;
 
     // Minimal bytecode that immediately returns (i.e. succeeds).
@@ -69,9 +68,7 @@ contract PectraTest is Test {
         }
         bytes memory target = validPubkey();
         vm.prank(address(pectra));
-        vm.expectRevert(
-            "Number of source validators must be less than 64, since Max EB is 2048 ETH"
-        );
+        vm.expectRevert("Number of source validators must be less than 64, since Max EB is 2048 ETH");
         pectra.batchConsolidation{value: count}(sources, target);
     }
 
@@ -82,12 +79,7 @@ contract PectraTest is Test {
         // Provide an invalid target pubkey (47 bytes instead of 48)
         bytes memory invalidTarget = new bytes(47);
         vm.prank(address(pectra));
-        vm.expectRevert(
-            abi.encodeWithSelector(
-                Pectra.InvalidTargetPubkeyLength.selector,
-                invalidTarget
-            )
-        );
+        vm.expectRevert(abi.encodeWithSelector(Pectra.InvalidTargetPubkeyLength.selector, invalidTarget));
         pectra.batchConsolidation{value: 1}(sources, invalidTarget);
     }
 
@@ -98,9 +90,7 @@ contract PectraTest is Test {
         sources[1] = validPubkey();
         bytes memory target = validPubkey();
         vm.prank(address(pectra));
-        vm.expectRevert(
-            "msg.value must be divisible by length of source validators"
-        );
+        vm.expectRevert("msg.value must be divisible by length of source validators");
         pectra.batchConsolidation{value: 3}(sources, target); // 3 wei not divisible by 2
     }
 
@@ -112,11 +102,7 @@ contract PectraTest is Test {
         bytes memory target = validPubkey();
         vm.prank(address(pectra));
         vm.expectEmit(false, false, false, true);
-        emit Pectra.ConsolidationFailed(
-            "Invalid source validator public key length",
-            address(pectra),
-            sources[0]
-        );
+        emit Pectra.ConsolidationFailed("Invalid source validator public key length", address(pectra), sources[0]);
         pectra.batchConsolidation{value: 1}(sources, target);
     }
 
@@ -129,11 +115,7 @@ contract PectraTest is Test {
         bytes memory target = validPubkey();
         vm.prank(address(pectra));
         vm.expectEmit(false, false, false, true);
-        emit Pectra.ConsolidationFailed(
-            "Consolidation failed",
-            address(pectra),
-            sources[0]
-        );
+        emit Pectra.ConsolidationFailed("Consolidation failed", address(pectra), sources[0]);
         pectra.batchConsolidation{value: 1}(sources, target);
         // Restore successful code.
         vm.etch(consolidationTarget, successCode);
@@ -198,11 +180,7 @@ contract PectraTest is Test {
         pubkeys[0] = new bytes(47);
         vm.prank(address(pectra));
         vm.expectEmit(false, false, false, true);
-        emit Pectra.SwitchFailed(
-            "Invalid validator public key length",
-            address(pectra),
-            pubkeys[0]
-        );
+        emit Pectra.SwitchFailed("Invalid validator public key length", address(pectra), pubkeys[0]);
         pectra.batchSwitch{value: 1}(pubkeys);
     }
 
@@ -280,10 +258,7 @@ contract PectraTest is Test {
         vm.prank(address(pectra));
         vm.expectEmit(false, false, false, true);
         emit Pectra.ExecutionLayerExitFailed(
-            "Invalid validator public key length",
-            address(pectra),
-            data[0][0],
-            data[0][1]
+            "Invalid validator public key length", address(pectra), data[0][0], data[0][1]
         );
         pectra.batchELExit{value: 1}(data);
     }
@@ -295,12 +270,7 @@ contract PectraTest is Test {
         data[0][1] = new bytes(7);
         vm.prank(address(pectra));
         vm.expectEmit(false, false, false, true);
-        emit Pectra.ExecutionLayerExitFailed(
-            "Invalid amount length",
-            address(pectra),
-            data[0][0],
-            data[0][1]
-        );
+        emit Pectra.ExecutionLayerExitFailed("Invalid amount length", address(pectra), data[0][0], data[0][1]);
         pectra.batchELExit{value: 1}(data);
     }
 
@@ -311,12 +281,7 @@ contract PectraTest is Test {
         data[0][1] = validAmount();
         vm.prank(address(pectra));
         vm.expectEmit(false, false, false, true);
-        emit Pectra.ExecutionLayerExitFailed(
-            "Execution layer exit failed",
-            address(pectra),
-            data[0][0],
-            data[0][1]
-        );
+        emit Pectra.ExecutionLayerExitFailed("Execution layer exit failed", address(pectra), data[0][0], data[0][1]);
         pectra.batchELExit{value: 1}(data);
         vm.etch(exitTarget, successCode);
     }
